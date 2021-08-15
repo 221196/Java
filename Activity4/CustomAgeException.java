201debacb07e742f51dc7ece29e8f7de8a052118
charmi zala <68703153+221196@users.noreply.github.com>
2021-08-15 22:11:02 +0530

    Create CustomAgeException.java

---
+++ Activity4/CustomAgeException.java
@@ -0,0 +1,28 @@
+package javaTrack;
+
+
+public class CustomAgeException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+
+	public CustomAgeException() {
+	
+	public CustomAgeException(String message) {
+		super(message);
+		
+	public CustomAgeException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomAgeException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public CustomAgeException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// TODO Auto-generated constructor stub
+	}
+
+}
