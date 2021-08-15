c7b8b2cd80e5a8b758248e7caf86e22f851648d8
charmi zala <68703153+221196@users.noreply.github.com>
2021-08-15 21:51:41 +0530

    Create DeluxeSeaViewRoom.java

---
+++ Activity3/Problem2/DeluxeSeaViewRoom.java
@@ -0,0 +1,8 @@
+public class DeluxeSeaViewRoom extends DeluxeRoom {
+
+	public DeluxeSeaViewRoom(String hotelName, int numberOfSqFeet, boolean hasTV, boolean hasWifi, int ratePerSqFeet) {
+		super(hotelName, numberOfSqFeet, hasTV, hasWifi, ratePerSqFeet);
+		// TODO Auto-generated constructor stub
+	}
+
+}
